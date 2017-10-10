17fa8f37b7ad9c0fdc3386ebd45f60026892c1c5
Marina <marinavegacodes@gmail.com>
2017-10-10 12:49:09 +0200

    Fix Authentication

--- authentication.rb
+++ authentication.rb
@@ -7,11 +7,12 @@ class Authentication
     @file_content = YAML::load_file('config.yaml')
   end
 
-def client
-  Twitter::REST::Client.new do |config|
-      config.consumer_key        = @file_content['consumer_key']
-      config.consumer_secret     = @file_content['consumer_secret']
-      config.access_token        = @file_content['access_token']
-      config.access_token_secret = @file_content['access_token_secret']
-    end
+  def client
+    Twitter::REST::Client.new do |config|
+        config.consumer_key        = @file_content['consumer_key']
+        config.consumer_secret     = @file_content['consumer_secret']
+        config.access_token        = @file_content['access_token']
+        config.access_token_secret = @file_content['access_token_secret']
+      end
+  end
 end
--- rosadiezbot.rb
+++ rosadiezbot.rb
@@ -1,5 +1,10 @@
 require 'rubygems'
 require 'twitter'
+require './authentication.rb'
 
 auth = Authentication.new
 client = auth.client
+
+client.search('viva el vino').each do |tweet|
+  puts "Yassss."
+end
